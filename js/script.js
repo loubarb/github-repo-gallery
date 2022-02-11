f5447df57342d2152f9e8f07050b3109e769a28f
loubarb <louis.barbati@gmail.com>
2022-02-11 17:28:55 -0500

    Added repo data to populate my repos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // div where profile info appears:
 const overview = document.querySelector(".overview");
 const username = "loubarb";
+const repoList = document.querySelector(".repo-list");
 
 async function gitInfo () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -10,6 +11,7 @@ async function gitInfo () {
 };
 gitInfo();
 
+// fetch profile info to display at top
 function displayUserInfo (data) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("user-info");
@@ -23,4 +25,24 @@ function displayUserInfo (data) {
                             <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
                         </div> `;
     overview.append(newDiv);
+    myRepos();
 };
+
+// fetch repos
+async function myRepos () {
+    const myInfo = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await myInfo.json();
+    console.log(repoData);
+    displayRepoInfo(repoData);
+};
+
+// repo info
+function displayRepoInfo (repos) {
+    for (const repo of repos) {
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(li);
+    }
+};
+
