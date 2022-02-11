4e58ec9a990144afe38754f531f9c3b57e2c1361
loubarb <louis.barbati@gmail.com>
2022-02-11 16:35:48 -0500

    Added json to pull data

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// div where profile info appears:
+const overview = document.querySelector(".overview");
+const username = "loubarb";
+
+async function gitInfo () {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    console.log(data);
+    displayUserInfo(data);
+};
+gitInfo();
+
+function displayUserInfo (data) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("user-info");
+    newDiv.innerHTML = `<figure>
+                            <img alt="user avatar" src=${data.avatar_url} />
+                        </figure>
+                        <div>
+                            <p><strong>Name:</strong> ${data.name}</p>
+                            <p><strong>Bio:</strong> ${data.bio}</p>
+                            <p><strong>Location:</strong> ${data.location}</p>
+                            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+                        </div> `;
+    overview.append(newDiv);
+};
